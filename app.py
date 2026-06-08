0a14bed566d1f232676771c227fabc5afcae750d
PRADNYA <pradnyakirwale4@gmail.com>
2026-06-08 18:41:06 +0530

    ui added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-  return {"message": "Flask Server Running Successfully"}
+  return render_template("index.html")
 
 if __name__ == "__main__":
  app.run(debug=True)
